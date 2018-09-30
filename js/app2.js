afa946a8c82b576e52b530858f8fdc8f643be9cd
Ramona Saintandre <thenewmona@gmail.com>
2018-09-29 21:18:16 -0400

    change html back to app2.js that is why it was not working

--- js/app2.js
+++ js/app2.js
@@ -15,45 +15,40 @@ let Enemy = function (x, y) {
 };
 
 Enemy.prototype.update = function (dt) {
-    this.x += 150 * dt;
-    if (this.x > ctx.canvas.width + this.width) {
-        this.x = -200 * Math.floor(Math.random() * 4) + 1;
-    } else {
-        this.x += 150 * dt;
+    this.x += 150 *dt;
+if (this.x > ctx.canvas.width + this.width){
+    this.x = -200*Math.floor(Math.random() *4) + 1;
+}else{
+    this.x += 150 *dt;
 
-        //enemy collision
+    // Enemy.prototype.collision = function () {
+    //     let enemyWidth = 95;
+    //     let enemyHeight = 65;
+    //     if (player.x + this.width > this.x &&
+    //         player.x < this.x + this.width &&
+    //         player.y + this.height > this.y &&
+    //         player.y < +this.height) {
+    //         reset();
+    //     }
 
+    //enemy collision
+//     if (collision(player.x, player.y, player.width, player.width, this.x, this.y, this.width, this.height,));
+// }else{
+//     this.collision = true;
 
+//     if (player) {
+//         player.x =202;
+//         player.y =400;
+//     }
+// }else{
+//     this.collision = false;
+// }
+// };    
 
-        // Enemy.prototype.collision = function () {
-        //     let enemyWidth = 95;
-        //     let enemyHeight = 65;
-        //     if (player.x + this.width > this.x &&
-        //         player.x < this.x + this.width &&
-        //         player.y + this.height > this.y &&
-        //         player.y < +this.height) {
-        //         reset();
-        //     }
-        // }
-        //player needes to reset when collision happens 
-
-        //     if (collision(player.x, player.y, player.width, player.width, this.x, this.y, this.width, this.height,));
-        // }else{
-        //     this.collision = true;
-
-        //     if (player) {
-        //         player.x =202;
-        //         player.y =400;
-        //     }
-        // }else{
-        //     this.collision = false;
-        // }
-        // };    
-
-        //     //resets players position 
+//     //resets players position 
 
 
-    }
+ }
 };
 
 Enemy.prototype.render = function () {
@@ -74,26 +69,26 @@ Player.prototype.render = function () {
 };
 
 Player.prototype.update = function (dt) {
-    // if (game && player.y < 40) {
-    //     game = false;
-    //     won();
-    // }
+// if (game && player.y < 40) {
+//     game = false;
+//     won();
+// }
 };
 
 Player.prototype.handleInput = function (direction) {
     //line 137 engine.js 
-    const horizontal = 101, //column
-        vertical = 83; //row
+    const horizontal = 101,//column
+    vertical = 83;//row
 
     if (direction === 'left' && this.x - horizontal >= 0) {
         this.x -= horizontal;
-    } else if (direction === 'right' && this.x + horizontal < ctx.canvas.width) {
+    } else if (direction === 'right'&& this.x + horizontal < ctx.canvas.width) {
         this.x += horizontal;
-    } else if (direction === 'down' && this.y + vertical < ctx.canvas.height - 200) {
+    } else if (direction === 'down' && this.y + vertical < ctx.canvas.height -200){
         this.y += vertical;
     } else if (direction === 'up' && this.y - vertical > 0 - player.height) {
         this.y -= vertical;
-
+    
     }
 }
 
@@ -118,16 +113,15 @@ const player = new Player(202, 400, 'images/char-princess-girl.png');
 
 let allEnemies = enemyPosition.map((y, index) => {
     return new Enemy((-200 * (index + 1)), y);
-
 });
 
-let won = function () {
+let won = function() {
     reset();
     console.log('won!');
 }
-
-function reset() {
-    allEnemies = [];
+ 
+function reset () {
+    allEnemies =[];
 }
 
 // function collision (px,py,pw,ph,ex,ey,ew,eh){
